1e87ad94b1cca226563cc7a68e2d3d6548478da2
myamyu <myamyu@gmail.com>
2015-12-28 01:22:48 +0900

    ログ関数の修正、ディレクトリもリポジトリ設定も無いときは、lita newではなく自前で設定を用意するようにした。（Redisがlocal固定だったから・・・）

--- bot.bash
+++ bot.bash
@@ -3,9 +3,8 @@
 function log()
 {
     local _now=$(date '+%Y-%m-%d_%H:%M:%S')
-    local _level="${1}"
-    local _msg="${2}"
-    echo "$_now | $_level | $_msg"
+    local _msg="${1}"
+    echo "$_now|$_msg"
 }
 
 declare -r BOT_SOURCE_DIR=/opt/lita/src
@@ -13,10 +12,23 @@ if [ -z $BOT_SOURCE_BRANCH ]; then
     BOT_SOURCE_BRANCH=master
 fi
 
-log 'INFO' 'Start Run Bot!'
+log 'Start Run Bot!'
 if [ ! -d $BOT_SOURCE_DIR ]; then
     if [ -z $BOT_SOURCE_REPOSITORY ]; then
-        lita new $BOT_SOURCE_DIR
+        mkdir $BOT_SOURCE_DIR
+        cat << EOF > $BOT_SOURCE_DIR/lita_config.rb
+Lita.configure do |config|
+  config.robot.name = "Lita"
+  config.robot.log_level = :info
+  config.robot.adapter = :shell
+	config.redis.host = "redis"
+	config.redis.port = ${REDIS_PORT_6379_TCP_PORT}
+end
+EOF
+        cat <<EOF > $BOT_SOURCE_DIR/Gemfile
+source "https://rubygems.org"
+gem "lita"
+EOF
     else
         git clone $BOT_SOURCE_REPOSITORY $BOT_SOURCE_DIR
     fi
@@ -31,5 +43,5 @@ fi
 
 bundle install
 
-log 'INFO' 'Run Bot!'
+log 'Run Bot!'
 lita
