130baf1728a88e46ecb0245b5d0350c1dc96f39a
myamyu <myamyu@gmail.com>
2015-12-31 02:51:00 +0900

    起動時のコマンドとコンテナ名を渡せるよう修正

--- DockerRunLitaBot.ps1
+++ DockerRunLitaBot.ps1
@@ -4,12 +4,15 @@ Param
 (
     [string]$tag="latest",
     [string]$repo,
-    [string]$branch
+    [string]$branch,
+    [string]$cmd,
+    [string]$name="lita_bot"
 )
 
 docker run `
+  -i -t `
   --link lita_redis:redis `
-  --name lita_bot `
+  --name $name `
   -e BOT_SOURCE_REPOSITORY=$repo `
   -e BOT_SOURCE_BRANCH=$branch `
-  "myamyu/lita:$tag"
+  "myamyu/lita:$tag" $cmd
